1e1bbed071ddc9a983b053ba067725d5b9ee2e55
Andrew <yszhouyx@outlook.com>
2020-03-06 14:36:47 +0800

    Andrew's commitment at 14:36 March 6 2020

---
+++ app/src/main/java/com/example/cardx/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.cardx;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbarMainActivity = findViewById(R.id.toolbar_main_activity);
+        setSupportActionBar(toolbarMainActivity);
+        final LinearLayout linearLayoutAddBoardMainActivity=findViewById(R.id.linearlayout_add_board_main_activity);
+        final LinearLayout linearLayoutAddCardMainActivity=findViewById(R.id.linearlayout_add_card_main_activity);
+        linearLayoutAddBoardMainActivity.setVisibility(View.GONE);
+        linearLayoutAddCardMainActivity.setVisibility(View.GONE);
+        FloatingActionButton fabAddMainActivity=findViewById(R.id.fab_add_main_activity);
+        fabAddMainActivity.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if (linearLayoutAddBoardMainActivity.getVisibility()==View.GONE&&linearLayoutAddCardMainActivity.getVisibility()==View.GONE)
+                {
+                    linearLayoutAddBoardMainActivity.setVisibility(View.VISIBLE);
+                    linearLayoutAddCardMainActivity.setVisibility(View.VISIBLE);
+                }
+                else
+                {
+                    linearLayoutAddBoardMainActivity.setVisibility(View.GONE);
+                    linearLayoutAddCardMainActivity.setVisibility(View.GONE);
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu_main_toolbar,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
+
+        return super.onOptionsItemSelected(item);
+    }
+}
